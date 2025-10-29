358d9e9e1f813b1528fcb1e5d1f03d03927358fe
Vallis-LFC <115032819+Vallis-LFC@users.noreply.github.com>
2025-10-28 22:48:59 -0400

    for A.3

---
+++ Dn.m
@@ -0,0 +1,19 @@
+%A.3
+
+function [D] = Dn(d,n)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+D1 = [0.5,0,-0.5*1i,0,0.5*1i,0,0.5];
+D2 = (1/(n.*pi)*sin((n*pi)/2));
+D3 = (1/(n.*pi)*sin((n*pi)/4));
+
+if (d==1)
+    D=D1;
+end
+if(d==2)
+    D=D2;
+end
+if(d==3)
+    D=D3;
+end
+
